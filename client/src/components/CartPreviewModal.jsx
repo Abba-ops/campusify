b5d4378ab70caff81e931008308d1a8fe4132a57
Jadesola Kajeyale <johnbabs182@gmail.com>
2024-02-28 22:21:45 +0100

    made changes

--- client/src/components/CartPreviewModal.jsx
+++ client/src/components/CartPreviewModal.jsx
@@ -1,167 +1,112 @@
 import React from "react";
-import { Alert, ListGroup } from "react-bootstrap";
-import Button from "react-bootstrap/Button";
-import Modal from "react-bootstrap/Modal";
-import Col from "react-bootstrap/esm/Col";
-import Image from "react-bootstrap/esm/Image";
-import Row from "react-bootstrap/esm/Row";
-import Stack from "react-bootstrap/esm/Stack";
+import {
+  Alert,
+  Button,
+  Col,
+  Image,
+  ListGroup,
+  Modal,
+  Row,
+} from "react-bootstrap";
+import Stack from "react-bootstrap/Stack";
 import { useSelector } from "react-redux";
 import { numberWithCommas } from "../utils/cartUtils";
+import StarRating from "./StarRating";
+import { Link } from "react-router-dom";
 
 export default function CartPreviewModal({ show, onHide, product }) {
   const cart = useSelector((state) => state.cart);
   const { cartItems } = cart;
 
+  const isCartEmpty = cartItems.length === 0;
+
   return (
     <Modal show={show} onHide={onHide} size="lg" centered>
-      <Row>
+      <Row xs={1} md={2} className="g-4">
         <Col lg={6}>
           <Modal.Body>
-            <Alert variant="success" className="border-0">
+            <Alert variant="success" className="border-0 rounded-0 mb-3">
               Product successfully added to your shopping cart
             </Alert>
-            <Row>
-              <Col xs={6}>
-                <Image src={product.imageUrl} fluid rounded />
-              </Col>
-              <Col xs={6}>
-                <h6 className="text-uppercase">{product.productName}</h6>
-                <div className="text-primary">
-                  &#8358;{numberWithCommas(product.price)}
-                </div>
-                <div>{product.category}</div>
-              </Col>
-            </Row>
+            <div className="mb-3" style={{ height: "100%" }}>
+              <Image src={product.imageUrl} fluid className="h-100" />
+            </div>
           </Modal.Body>
         </Col>
         <Col lg={6}>
-          <Modal.Header>
-            <Modal.Title as={"h6"} className="text-uppercase">
-              There are{" "}
-              {cartItems.reduce((acc, item) => acc + item.quantity, 0)} items in
-              your cart
-            </Modal.Title>
-          </Modal.Header>
           <Modal.Body>
-            <ListGroup>
-              <ListGroup.Item>
-                <Row>
-                  <Col xs={6}>
-                    {cartItems.reduce((acc, item) => acc + item.quantity, 0)}{" "}
-                    items
-                  </Col>
-                  <Col xs={6} className="text-end  text-primary">
-                    &#8358;{numberWithCommas(cart.itemsPrice)}
-                  </Col>
-                </Row>
-              </ListGroup.Item>
-              <ListGroup.Item>
-                <Row>
-                  <Col xs={6}>Shipping</Col>
-                  <Col xs={6} className="text-end  text-primary">
-                    &#8358;{numberWithCommas(cart.shippingPrice)}
-                  </Col>
-                </Row>
-              </ListGroup.Item>
-              <ListGroup.Item>
-                <Row>
-                  <Col xs={6}>Taxes</Col>
-                  <Col xs={6} className="text-end  text-primary">
-                    &#8358;{numberWithCommas(cart.taxPrice)}
-                  </Col>
-                </Row>
-              </ListGroup.Item>
-              <ListGroup.Item>
-                <Row>
-                  <Col xs={6}>Total</Col>
-                  <Col xs={6} className="text-end  text-primary">
-                    &#8358;{numberWithCommas(cart.totalPrice)}
-                  </Col>
-                </Row>
-              </ListGroup.Item>
-            </ListGroup>
+            <Row className="mb-3">
+              <Col
+                xs={12}
+                className="d-flex flex-column justify-content-center">
+                <h6 className="text-uppercase mb-3">{product.productName}</h6>
+                <div className="mb-2">
+                  <StarRating value={product.rating} size={16} />
+                </div>
+                <h6 className="text-primary">
+                  &#8358;{numberWithCommas(product.price)}
+                </h6>
+              </Col>
+            </Row>
+            <Modal.Title as="h6" className="text-uppercase mb-3">
+              {cartItems.length === 1 ? "1 item" : `${cartItems.length} items`}{" "}
+              in your cart
+            </Modal.Title>
+            <div className="mb-4">
+              <ListGroup variant="flush">
+                <ListGroup.Item>
+                  <ListGroupItems
+                    label="Total items"
+                    value={numberWithCommas(cart.itemsPrice)}
+                  />
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <ListGroupItems
+                    label="Shipping"
+                    value={numberWithCommas(cart.shippingPrice)}
+                  />
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <ListGroupItems
+                    label="Taxes"
+                    value={numberWithCommas(cart.taxPrice)}
+                  />
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <ListGroupItems
+                    label="Total"
+                    value={numberWithCommas(cart.totalPrice)}
+                  />
+                </ListGroup.Item>
+              </ListGroup>
+            </div>
+            <Stack direction="vertical" gap={3} className="mt-4">
+              <Button
+                disabled={isCartEmpty}
+                className="text-uppercase w-100"
+                variant="dark"
+                onClick={onHide}>
+                Continue Shopping
+              </Button>
+              <Link to="/checkout" className="text-decoration-none">
+                <Button
+                  disabled={isCartEmpty}
+                  className="text-uppercase w-100"
+                  variant="dark">
+                  Proceed to Checkout
+                </Button>
+              </Link>
+            </Stack>
           </Modal.Body>
-          <Modal.Footer>
-            <Button
-              size="sm"
-              disabled={cartItems.length === 0}
-              className="rounded-0 text-uppercase raleway"
-              variant="dark">
-              Continue Shopping
-            </Button>
-            <Button
-              size="sm"
-              disabled={cartItems.length === 0}
-              className="rounded-0 text-uppercase raleway"
-              variant="dark">
-              Proceed to Checkout
-            </Button>
-          </Modal.Footer>
         </Col>
       </Row>
-      {/* <Row>
-        <Col lg={6} className="p-3">
-          <Row>
-            <Modal.Title className="text-success fw-light fs-5 mb-3">
-              Product successfully added to your shopping cart
-            </Modal.Title>
-            <Col lg={6}>
-              <Image src={product.imageUrl} fluid />
-            </Col>
-            <Col lg={6}>
-              <h6>{product.productName}</h6>
-              <h6 >&#8358;{product.price}</h6>
-              <p>
-                Quantity: <span>{cart.quantity}</span>
-              </p>
-            </Col>
-          </Row>
-        </Col>
-        <Col lg={6} className="border-start">
-          <Modal.Header>
-            <Modal.Title className="raleway fw-light">
-              There are{" "}
-              {cartItems.reduce((acc, item) => acc + item.quantity, 0)} items in
-              your cart
-            </Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <p >
-              Total products:{" "}
-              <span className="text-primary">&#8358;{cart.itemsPrice}</span>
-            </p>
-            <p >
-              Total shipping:{" "}
-              <span className="text-primary">&#8358;{cart.shippingPrice}</span>
-            </p>
-            <p >
-              Taxes:{" "}
-              <span className="text-primary">&#8358;{cart.taxPrice}</span>
-            </p>
-            <p >
-              Total:{" "}
-              <span className="text-primary">&#8358;{cart.totalPrice}</span>
-            </p>
-          </Modal.Body>
-          <Stack direction="horizontal" gap={3} className="mb-4">
-            <Button
-              size="sm"
-              disabled={cartItems.length === 0}
-              className="rounded-0 text-uppercase raleway"
-              variant="dark">
-              Continue Shopping
-            </Button>
-            <Button
-              size="sm"
-              disabled={cartItems.length === 0}
-              className="rounded-0 text-uppercase raleway"
-              variant="dark">
-              Proceed to Checkout
-            </Button>
-          </Stack>
-        </Col>
-      </Row> */}
     </Modal>
   );
 }
+
+const ListGroupItems = ({ label, value }) => (
+  <div className="d-flex justify-content-between align-items-center mb-2">
+    <span className="text-muted">{label}</span>
+    <h6 className="text-primary">&#8358;{value}</h6>
+  </div>
+);
--- client/src/components/ProductPreviewModal.jsx
+++ client/src/components/ProductPreviewModal.jsx
@@ -1,4 +1,4 @@
-import { RiShoppingBag2Fill } from "react-icons/ri";
+import React from "react";
 import Button from "react-bootstrap/esm/Button";
 import Image from "react-bootstrap/esm/Image";
 import Col from "react-bootstrap/esm/Col";
@@ -11,10 +11,11 @@ import { addToCart } from "../features/cartSlice";
 import AddedToCartModal from "./CartPreviewModal";
 import { numberWithCommas } from "../utils/cartUtils";
 import Stack from "react-bootstrap/esm/Stack";
+import StarRating from "./StarRating";
 
 export default function ProductPreviewModal({ product, show, handleClose }) {
-  const [quantity, setQuantity] = useState(1);
   const [showAddedModal, setShowAddedModal] = useState(false);
+  const [quantity, setQuantity] = useState(1);
 
   const dispatch = useDispatch();
 
@@ -27,27 +28,36 @@ export default function ProductPreviewModal({ product, show, handleClose }) {
     handleAddToCartShow();
   };
 
+  const isOutOfStock = product.countInStock <= 0;
+
   return (
     <>
       <Modal show={show} onHide={handleClose} centered size="lg">
-        <Row>
-          <Col lg={6} className="d-flex">
-            <Image src={product.imageUrl} fluid rounded />
+        <Row className="align-items-stretch">
+          <Col lg={6} className="d-flex align-items-center">
+            <Image
+              src={product.imageUrl}
+              fluid
+              rounded
+              style={{ height: "100%" }}
+            />
           </Col>
           <Col lg={6}>
-            <Modal.Header>
-              <Modal.Title className="text-uppercase">
+            <Modal.Body>
+              <Modal.Title className="text-uppercase mb-3">
                 {product.productName}
               </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <Modal.Title className="text-primary">
+              <StarRating value={product.rating} size={24} />
+              <p className="text-muted mb-3">{product.reviewCount} Reviews</p>
+              <Modal.Title className="text-primary mb-4">
                 &#8358;{numberWithCommas(product.price)}
               </Modal.Title>
-              <p className="my-3 text-muted">{product.productDescription}</p>
-              <Row>
+              <p className="my-3">{product.productDescription}</p>
+              <Row className="mb-3">
                 <Col>
-                  {product.countInStock > 0 && (
+                  {isOutOfStock ? (
+                    <p className="text-danger">Out of Stock</p>
+                  ) : (
                     <Form.Select
                       onChange={(e) => setQuantity(Number(e.target.value))}>
                       {[...Array(product.countInStock).keys()].map((x) => (
@@ -59,25 +69,25 @@ export default function ProductPreviewModal({ product, show, handleClose }) {
                   )}
                 </Col>
               </Row>
+              <Stack direction="horizontal" gap={3} className="mt-4">
+                <Button
+                  variant="primary"
+                  onClick={handleClose}
+                  className="text-white text-uppercase">
+                  Close
+                </Button>
+                <Button
+                  variant="dark"
+                  onClick={addToCartHandler}
+                  className="text-uppercase"
+                  disabled={isOutOfStock}>
+                  {isOutOfStock ? "Out of Stock" : "Add to Cart"}
+                </Button>
+              </Stack>
             </Modal.Body>
-            <Modal.Footer>
-              <Button
-                variant="primary"
-                onClick={handleClose}
-                className="text-white text-uppercase">
-                Close
-              </Button>
-              <Button
-                variant="dark"
-                onClick={addToCartHandler}
-                className="text-uppercase">
-                Add to cart
-              </Button>
-            </Modal.Footer>
           </Col>
         </Row>
       </Modal>
-
       <AddedToCartModal
         product={product}
         show={showAddedModal}
