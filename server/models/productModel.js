f7bc32d9944a56d5f959c99894d1537a1bd0461b
Jadesola Kajeyale <johnbabs182@gmail.com>
2024-03-15 14:32:40 +0100

    made more changes

--- server/models/productModel.js
+++ server/models/productModel.js
@@ -4,7 +4,6 @@ const subcategorySchema = new Schema({
   name: {
     type: String,
     required: true,
-    unique: true,
     trim: true,
     lowercase: true,
   },
@@ -14,7 +13,6 @@ const categorySchema = new Schema({
   name: {
     type: String,
     required: true,
-    unique: true,
     trim: true,
     lowercase: true,
   },
