b74e789ef90b126456c0516b85fa3c4e490cba78
Jadesola Kajeyale <johnbabs182@gmail.com>
2024-04-24 12:36:48 +0100

    made changes

--- client/public/serviceworker.js
+++ client/public/serviceworker.js
@@ -3,7 +3,7 @@ const urlsToCache = ["index.html", "offline.html"];
 
 const self = this;
 
-// Service Worker Installation
+// Install SW
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
@@ -14,7 +14,7 @@ self.addEventListener("install", (event) => {
   );
 });
 
-// Intercept and respond to fetch requests
+// Listen for requests
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then(() => {
@@ -23,9 +23,11 @@ self.addEventListener("fetch", (event) => {
   );
 });
 
-// Service Worker Activation
+// Activate the SW
 self.addEventListener("activate", (event) => {
-  const cacheWhitelist = [CACHE_NAME];
+  const cacheWhitelist = [];
+  cacheWhitelist.push(CACHE_NAME);
+
   event.waitUntil(
     caches.keys().then((cacheNames) =>
       Promise.all(
