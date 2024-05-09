eee3aa4652067447c4c18b2e1467306cd666bd01
Jadesola Kajeyale <johnbabs182@gmail.com>
2024-05-09 21:27:33 +0100

    changed font

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -23,7 +23,7 @@ export default function About() {
           </Col>
           <Col lg={6}>
             <div>
-              <h2 className="text-uppercase">Explore Our Campus Marketplace</h2>
+              <h4 className="text-uppercase">Explore Our Campus Marketplace</h4>
               <p>
                 At Campusify, we're dedicated to revolutionizing the way
                 university students engage in commerce within their campus
@@ -32,7 +32,7 @@ export default function About() {
                 specifically to the unique needs and dynamics of university
                 life.
               </p>
-              <h3 className="text-uppercase">Our Mission</h3>
+              <h4 className="text-uppercase">Our Mission</h4>
               <p>
                 Our mission is simple yet powerful: to empower student
                 entrepreneurs by providing them with the tools and opportunities
@@ -41,7 +41,7 @@ export default function About() {
                 businesses, and we're here to support them every step of the
                 way.
               </p>
-              <h3 className="text-uppercase">Why Campusify?</h3>
+              <h4 className="text-uppercase">Why Campusify?</h4>
               <p>
                 Traditional marketplaces often fall short when it comes to
                 catering to the specific needs of campus communities. That's
@@ -49,7 +49,7 @@ export default function About() {
                 students face in showcasing and monetizing their products and
                 services within their campus, and we're here to bridge that gap.
               </p>
-              <h3 className="text-uppercase">Bridging the Gap</h3>
+              <h4 className="text-uppercase">Bridging the Gap</h4>
               <p>
                 With Campusify, students no longer have to rely on generic
                 e-commerce platforms that may not understand the dynamics of
@@ -58,7 +58,7 @@ export default function About() {
                 user-friendly interface to register, showcase, and sell their
                 products or services within their campus community.
               </p>
-              <h3 className="text-uppercase">Fostering Entrepreneurship</h3>
+              <h4 className="text-uppercase">Fostering Entrepreneurship</h4>
               <p>
                 But Campusify is more than just a marketplace—it's a catalyst
                 for entrepreneurship. By providing students with a platform to
@@ -69,7 +69,7 @@ export default function About() {
                 contributing to the development of vibrant entrepreneurial
                 ecosystems within universities.
               </p>
-              <h3 className="text-uppercase">Join Us</h3>
+              <h4 className="text-uppercase">Join Us</h4>
               <p>
                 Whether you're a student looking to turn your passion into
                 profit or a campus community member eager to support local
